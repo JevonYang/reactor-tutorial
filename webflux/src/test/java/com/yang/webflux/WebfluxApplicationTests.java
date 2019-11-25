9cd4f4fa82bad9bcc3c4eaf10c94b966421fc252
JevonYang <yangzijing08@gmail.com>
2019-11-25 22:37:41 +0800

    websocket例子

--- webflux/src/test/java/com/yang/webflux/WebfluxApplicationTests.java
+++ webflux/src/test/java/com/yang/webflux/WebfluxApplicationTests.java
@@ -6,6 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.reactive.socket.WebSocketMessage;
+import org.springframework.web.reactive.socket.client.ReactorNettyWebSocketClient;
+import org.springframework.web.reactive.socket.client.WebSocketClient;
+import reactor.core.publisher.Flux;
+
+import java.net.URI;
+import java.time.Duration;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -17,6 +24,18 @@ public class WebfluxApplicationTests {
     @Test
     public void contextLoads() {
         redisTemplate.convertAndSend("hello", "world");
+    }
+
+    @Test
+    public void websocket() {
+        // https://www.websocket.org/echo.html
+        final WebSocketClient client = new ReactorNettyWebSocketClient();
+        client.execute(URI.create("ws://localhost:8081/echo"), session ->
+                session.send(Flux.just(session.textMessage("world")))
+                        .thenMany(session.receive().take(1).map(WebSocketMessage::getPayloadAsText))
+                        .doOnNext(System.out::println)
+                        .then())
+                .block(Duration.ofMillis(5000));
     }
 
 }
